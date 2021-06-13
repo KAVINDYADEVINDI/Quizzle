7677ee7f5f0627a3aa31d508354eccf34acbc321
KAVINDYADEVINDI <kavindyadewindi12345678@gmail.com>
2021-06-13 14:15:53 -0700

    set options

--- App.tsx
+++ App.tsx
@@ -1,6 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View,ImageBackground } from 'react-native';
+import Quiz from './screens/quiz';
+import Result from './screens/result';
 import Welcome from './screens/Welcome';
 
 export default function App() {
@@ -8,7 +10,9 @@ export default function App() {
     <ImageBackground source={ {uri: 'https://images.unsplash.com/photo-1582769923195-c6e60dc1d8dc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTZ8fHxlbnwwfHx8fA%3D%3D&w=1000&q=80'} } 
     style={styles.backgroundImage}>
       <View style={styles.container}>
-        <Welcome/>
+        {/* <Welcome/> */}
+        <Quiz/>
+        {/* <Result/> */}
         {/* <StatusBar style="auto" /> */}
       </View>
     </ImageBackground>
--- screens/quiz.tsx
+++ screens/quiz.tsx
@@ -3,12 +3,12 @@ import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 const Quiz=()=>{
     return (
-       <View>
-            <View>
+       <View style={styles.container}>
+            <View style={styles.top}>
                 <Text>Question 1</Text>
             </View>
 
-            <View>
+            <View  style={styles.options}>
                 <TouchableOpacity>
                     <Text>Option 1</Text>
                 </TouchableOpacity>
@@ -23,7 +23,7 @@ const Quiz=()=>{
                 </TouchableOpacity>
             </View>
 
-            <View>
+            <View style={styles.bottom}>
                 <TouchableOpacity>
                     <Text>SKIP</Text>
                 </TouchableOpacity>
@@ -38,4 +38,23 @@ const Quiz=()=>{
 
 export default Quiz;
 
-const styles= StyleSheet.create({});
+const styles= StyleSheet.create({
+    container:{
+        padding: 12,
+        height:'100%,'
+    },
+    top:{
+        marginVertical:16,
+    },
+    options:{
+        marginVertical:16,
+        flex:1,
+    },
+    bottom:{
+        marginBottom:12,
+        paddingVertical:16,
+        justifyContent:'space-between',
+        flexDirection:'row',
+    }
+
+});
