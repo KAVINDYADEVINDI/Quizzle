db038251714d8b9d952798ed07cc78a715d53b77
KAVINDYADEVINDI <kavindyadewindi12345678@gmail.com>
2021-06-13 01:11:25 -0700

    welcome page edit

--- screens/Welcome.tsx
+++ screens/Welcome.tsx
@@ -1,23 +1,36 @@
 import React from 'react';
-import { StyleSheet, Text, View,TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity,Image } from 'react-native';
 import Title from '../components/title';
 
-const Welcome=()=>{
+const Welcome = () => {
     return (
         <View>
-            <Title/>
+            <Title />
 
-            {/* <view>
-
-            </view> */}
+            <View style={styles.bannerContainer}>
+                <Image source={ {uri: 'https://storyset.com/illustration/exams/pana'} } 
+                    style={styles.banner}
+                    resizeMode="contain"
+                />
+            </View>
             <TouchableOpacity>
                 <Text>Get Start</Text>
             </TouchableOpacity>
         </View>
-        
+
     );
 };
 
 export default Welcome;
 
-const styles= StyleSheet.create({});
+const styles = StyleSheet.create({
+    banner:{
+        height:300,
+        width:300,
+    },
+    bannerContainer:{
+        justifyContent:'center',
+        alignItems:'center',
+        
+    }
+});
