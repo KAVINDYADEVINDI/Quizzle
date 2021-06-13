dc9674bd6a0aa3ebdea72e6c2f4362b68f5e1b7f
KAVINDYADEVINDI <kavindyadewindi12345678@gmail.com>
2021-06-13 00:33:27 -0700

    init commiti

---
+++ screens/Welcome.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+//get image
+export const welcomeAssets=require("../../assets/images/wel.png");
+
+const Welcome=()=>{
+    return (
+   
+    );
+};
+
+
+
+
+export default Welcome;
