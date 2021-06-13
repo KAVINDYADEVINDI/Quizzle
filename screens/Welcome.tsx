89e11e32f02e997066ea110032c10400093bc86d
KAVINDYADEVINDI <kavindyadewindi12345678@gmail.com>
2021-06-13 00:36:35 -0700

    edit first

--- screens/Welcome.tsx
+++ screens/Welcome.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-
+import { StyleSheet, Text, View } from 'react-native';
 
 //get image
 export const welcomeAssets=require("../../assets/images/wel.png");
 
 const Welcome=()=>{
     return (
-   
+   <Text>hello</Text>
     );
 };
 
