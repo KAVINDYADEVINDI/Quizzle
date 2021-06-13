e2db63fa98dd1d0053993b90b32cea6fc0af1824
KAVINDYADEVINDI <kavindyadewindi12345678@gmail.com>
2021-06-13 00:46:30 -0700

    addd title.tsx

---
+++ components/title.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const Welcome=()=>{
+    return (
+        <view>
+            <Text>hello result</Text>
+        </view>
+        
+    );
+};
+
+export default Welcome;
+
+const styles= StyleSheet.create({});
