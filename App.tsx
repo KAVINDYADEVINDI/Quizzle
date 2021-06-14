a0ea872dcb244753317168b039a86e6aaebc28a0
KAVINDYADEVINDI <kavindyadewindi12345678@gmail.com>
2021-06-13 23:41:44 -0700

    edit index

--- App.tsx
+++ App.tsx
@@ -1,24 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Quiz from './screens/quiz';
-import Result from './screens/result';
-import Welcome from './screens/Welcome';
+import { NavigationContainer } from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import MyStack from "./navigation";
+import Quiz from "./screens/quiz";
+import Result from "./screens/result";
+import Welcome from "./screens/Welcome";
 
 export default function App() {
   return (
-    
-      <View >
-        <Welcome/>
-        {/* <Quiz/> */}
-        {/* <Result/> */}
-        {/* <StatusBar style="auto" /> */}
-      </View>
-   
+    <View>
+      <NavigationContainer>
+        <MyStack/>
+      </NavigationContainer>
+      <Welcome />
+      {/* <Quiz/> */}
+      {/* <Result/> */}
+      {/* <StatusBar style="auto" /> */}
+    </View>
   );
 }
 
-const styles = StyleSheet.create({
-  
- 
-});
+const styles = StyleSheet.create({});
--- navigation/index.tsx
+++ navigation/index.tsx
@@ -1,16 +1,22 @@
-import {createStackNavigator} from '@react-navigation/stack';
-import Quiz from '../screens/quiz';
-import Result from '../screens/result';
-import Welcome from '../screens/Welcome';
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import Quiz from "../screens/quiz";
+import Result from "../screens/result";
+import Welcome from "../screens/Welcome";
 
 const Stack = createStackNavigator();
 
 function MyStack() {
   return (
-    <Stack.Navigator initialRouteName="Welcome">
-      <Stack.Screen name="Welcome" component={Welcome} />
-      <Stack.Screen name="Quiz" component={Quiz} />
-      <Stack.Screen name="Result" component={Result} />
-    </Stack.Navigator>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Welcome">
+        <Stack.Screen name="Welcome" component={Welcome} />
+        <Stack.Screen name="Quiz" component={Quiz} />
+        <Stack.Screen name="Result" component={Result} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
+
+export default MyStack;
