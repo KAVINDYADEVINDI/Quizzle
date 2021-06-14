d48e3606caaf230d240e9d197ecac1ce34fcff05
KAVINDYADEVINDI <kavindyadewindi12345678@gmail.com>
2021-06-14 01:24:01 -0700

    add css

--- components/title.tsx
+++ components/title.tsx
@@ -1,25 +1,28 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
-const Title=()=>{
-    return (
-        <View style={styles.text}>
-            <Text style={styles.textStyle}>Quizzora</Text>
-        </View>
-        
-    );
+const Title = () => {
+  return (
+    <View style={styles.text}>
+      <Text style={styles.textStyle}>Quizzora</Text>
+    </View>
+  );
 };
 
 export default Title;
 
-const styles= StyleSheet.create({
-    text:{
-        paddingTop:60,
-        justifyContent:'center',
-        alignItems:'center',
-    },
-    textStyle:{
-        fontSize:40,
-        fontWeight:'600'
-    }
+const styles = StyleSheet.create({
+  text: {
+    paddingTop: 60,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  textStyle: {
+    color: "#e0a81b",
+    fontSize: 40,
+    fontWeight: "bold",
+    textShadowOffset: { width: 2, height: 3 },
+    textShadowRadius: 10,
+    textShadowColor: "#e0a81b",
+  },
 });
--- screens/Welcome.tsx
+++ screens/Welcome.tsx
@@ -4,46 +4,61 @@ import Title from '../components/title';
 
 const Welcome = ({navigation}) => {
     return (
-        <View style={{width:"100%",height:"100%"}}>
-            <ImageBackground   source={ {uri: 'https://images.unsplash.com/photo-1582769923195-c6e60dc1d8dc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTZ8fHxlbnwwfHx8fA%3D%3D&w=1000&q=80'} } 
-            style={styles.backgroundImage}>
-                <View >
-                    <Title />
-                    <View style={styles.bannerContainer}>
-                        <Image source={ {uri: 'https://cdn3d.iconscout.com/3d/premium/thumb/question-mark-2891357-2409768@0.png'} } 
-                            style={styles.banner}
-                            resizeMode="contain"
-                        />
-                    </View>
-                    <TouchableOpacity onPress={()=>navigation.navigate("Quiz")}>
-                        <Text>Get Start</Text>
-                    </TouchableOpacity>
-                </View>
-            </ImageBackground>
-        </View>
-
+      <View style={{ width: "100%", height: "100%" }}>
+        <ImageBackground
+          source={{
+            uri: "https://images.unsplash.com/photo-1582769923195-c6e60dc1d8dc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTZ8fHxlbnwwfHx8fA%3D%3D&w=1000&q=80",
+          }}
+          style={styles.backgroundImage}
+        >
+          <View>
+            <Title />
+            <View style={styles.bannerContainer}>
+              <Image
+                source={{
+                  uri: "https://cdn3d.iconscout.com/3d/premium/thumb/question-mark-2891357-2409768@0.png",
+                }}
+                style={styles.banner}
+                resizeMode="contain"
+              />
+            </View>
+            <TouchableOpacity
+              style={styles.Btn}
+              onPress={() => navigation.navigate("Quiz")}
+            >
+              <Text style={styles.BtnText}>Get Start</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => navigation.navigate("Quiz")}>
+              <Text>How to Play?</Text>
+            </TouchableOpacity>
+          </View>
+        </ImageBackground>
+      </View>
     );
 };
 
 export default Welcome;
 
 const styles = StyleSheet.create({
-    
-    banner:{
-        height:300,
-        width:300,
-    },
-    bannerContainer:{
-        justifyContent:'center',
-        alignItems:'center',
-        
-    },
-    backgroundImage: {
-        flex:1,  
-        resizeMode: 'cover',
-        width:"100%",
-        height:"100%" // or 'stretch'
-    }
-  
-    
+  banner: {
+    height: 300,
+    width: 300,
+  },
+  bannerContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  backgroundImage: {
+    flex: 1,
+    resizeMode: "cover",
+    width: "100%",
+    height: "100%", // or 'stretch'
+  },
+  Btn: {
+    width: "80%",
+  },
+  BtnText: {
+    width: "80%",
+  },
+  HowPlayBtn: {},
 });
