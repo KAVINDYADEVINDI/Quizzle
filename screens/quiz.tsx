9e281e2afed876f9f7a63ddd8a68ba97a7d22fef
KAVINDYADEVINDI <kavindyadewindi12345678@gmail.com>
2021-06-14 14:56:38 -0700

    css for quiz and wel

--- screens/quiz.tsx
+++ screens/quiz.tsx
@@ -22,21 +22,26 @@ const Quiz = ({navigation}) => {
           </View>
 
           <View style={styles.top}>
-            <Text style={styles.topText}>Question 1</Text>
+            <Text style={styles.topText}>Question 1/5</Text>
           </View>
 
+          <View style={styles.question}>
+            <Text style={styles.questionText}>
+              Q.Where is Samsung group located?
+            </Text>
+          </View>
           <View style={styles.options}>
-            <TouchableOpacity>
-              <Text>a.Option 1</Text>
+            <TouchableOpacity style={styles.option}>
+              <Text style={styles.optionText}>Seoul</Text>
             </TouchableOpacity>
-            <TouchableOpacity>
-              <Text>b.Option 2</Text>
+            <TouchableOpacity style={styles.option}>
+              <Text style={styles.optionText}>Senegal</Text>
             </TouchableOpacity>
-            <TouchableOpacity>
-              <Text>c.Option 3</Text>
+            <TouchableOpacity style={styles.option}>
+              <Text style={styles.optionText}>Colarado</Text>
             </TouchableOpacity>
-            <TouchableOpacity>
-              <Text>d.Option 4</Text>
+            <TouchableOpacity style={styles.option}>
+              <Text style={styles.optionText}>California</Text>
             </TouchableOpacity>
           </View>
 
@@ -77,6 +82,8 @@ const styles = StyleSheet.create({
   container: {
     paddingTop: 40,
     padding: 12,
+    width: "100%",
+    height: "100%",
   },
   banner: {
     height: 300,
@@ -90,15 +97,32 @@ const styles = StyleSheet.create({
   top: {
     justifyContent: "center",
     alignItems: "center",
-
     marginVertical: 16,
   },
   topText: {
     fontSize: 20,
     fontWeight: "bold",
   },
+  question: {
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+  },
+  questionText: {
+    fontSize: 18,
+  },
   options: {
+    paddingHorizontal: 20,
     marginVertical: 16,
+  },
+  option: {
+    paddingVertical: 12,
+    marginVertical: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#af46eb",
+  },
+  optionText: {
+    fontSize: 16,
   },
   bottom: {
     marginBottom: 12,
@@ -106,17 +130,15 @@ const styles = StyleSheet.create({
     paddingVertical: 26,
     justifyContent: "space-between",
     flexDirection: "row",
-    
   },
   button: {
     justifyContent: "center",
     alignItems: "center",
     textAlign: "center",
-    borderRadius: 20,
-
+    borderRadius: 10,
   },
   buttonText: {
     paddingHorizontal: 20,
-    paddingVertical:7,
+    paddingVertical: 7,
   },
 });
