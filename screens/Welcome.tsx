e630c5ae33b2e85843682084be82046146d04ae1
KAVINDYADEVINDI <kavindyadewindi12345678@gmail.com>
2021-06-15 13:12:07 -0700

    editinng welcome

--- screens/Welcome.tsx
+++ screens/Welcome.tsx
@@ -41,7 +41,7 @@ const Welcome = ({navigation}) => {
             </TouchableOpacity>
 
             <TouchableOpacity style={styles.HowPlayBtn}>
-              <Text style={styles.HowPlayBtnText}>How to Play?</Text>
+              <Text style={styles.HowPlayBtnText}>Improve your app building skills and upgrade your personal growth</Text>
             </TouchableOpacity>
           </View>
         </ImageBackground>
